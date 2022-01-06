2f1b06d463972401bcd4402f9531efcb2940f46a
Noah Day <noah.day@adelaide.edu.au>
2022-01-06 17:52:49 +1030

    SWH and comparisons

--- miz.m
+++ miz.m
@@ -6,8 +6,12 @@ close all
 addpath functions
 addpath packages/bedmap2_toolbox_v4
 
-fsd_max = 50;
-
+fsd_max = 10;
+movie_miz = 0; % 0 is off, 1 is on
+average_miz_switch = 0; 
+sum_miz_switch = 1;
+swh_ice_miz_switch = 1;
+iceedge = 0;
 %% Preamble
 user = 'noahday'; %a1724548, noahday, Noah
 case_name = '8month';
@@ -36,7 +40,6 @@ year = 2005;
 date = sprintf('%d-0%d-0%d', year, month, day);
 map_type = 'eqaazim'; %cassini
 
-%% Loading data
 % Load the grid, ice shelf, and MIZ statistics.
 
 % Grid
@@ -58,120 +61,210 @@ shelf = 0.0*(shelf == 1);
 shelf(shelf==0) = NaN; % 1 iceshelf
 [latshelf,lonshelf] = bedmap2_data('latlon');
 
-% CICE data in mean
-[len, wid] = size(lat);
-% 1. FSD radius < fsd_max
-dim = 2;
-variable = "fsdrad";
-fsd_data = aggregate_data(case_name,date,datapoints,variable,dim);
-idx = fsd_data > fsd_max; 
-fsd_miz = fsd_data;
-fsd_miz(idx) = 0.0;
-idx = fsd_miz > eps;
-fsd_miz(idx) = 100.0;
 
-% 2. SWH > 0
+%% 1. CICE data in mean
+if average_miz_switch == 1
+    date = sprintf('%d-0%d-0%d', year, month, day);
+    [len, wid] = size(lat);
+    % a. FSD radius < fsd_max
+    dim = 2;
+    variable = "fsdrad";
+    fsd_data = aggregate_data(case_name,date,datapoints,variable,dim);
+    idx = fsd_data > fsd_max; 
+    fsd_miz = fsd_data;
+    fsd_miz(idx) = 0.0;
+    idx = fsd_miz > eps;
+    fsd_miz(idx) = 100.0;
 
-% dim = 2;
-% variable = "wave_sig_ht";
-% swh_data = aggregate_data(case_name,date,datapoints,variable,dim);
-% idx = swh_data > 0;
-% swh_miz = swh_data;
-% swh_miz(~idx) = 0.0;
-% idx = swh_miz > eps;
-% swh_miz(idx) = 50.0;
+    % b. SWH > 0
 
-% 3. dafsd_wave > 0
+    % dim = 2;
+    % variable = "wave_sig_ht";
+    % swh_data = aggregate_data(case_name,date,datapoints,variable,dim);
+    % idx = swh_data > 0;
+    % swh_miz = swh_data;
+    % swh_miz(~idx) = 0.0;
+    % idx = swh_miz > eps;
+    % swh_miz(idx) = 50.0;
 
-% dim = 3;
-% variable = "dafsd_wave";
-% wave_data = aggregate_data(case_name,date,datapoints,variable,dim);
-% idx = wave_data > 0;
-% dafsd_miz = wave_data;
-% dafsd_miz(~idx) = 0.0;
-% idx = dafsd_miz > eps;
-% dafsd_miz(idx) = 10.0;
+    % c. dafsd_wave > 0
 
-% Combining the datasets 
-total_miz = fsd_miz;% + swh_miz; %+ dafsd_miz;
+    % dim = 3;
+    % variable = "dafsd_wave";
+    % wave_data = aggregate_data(case_name,date,datapoints,variable,dim);
+    % idx = wave_data > 0;
+    % dafsd_miz = wave_data;
+    % dafsd_miz(~idx) = 0.0;
+    % idx = dafsd_miz > eps;
+    % dafsd_miz(idx) = 10.0;
 
-
-% CICE data at each day during the season - movie
-video_name = strcat(variable, '_', case_name, '_', '2021_11_30', '.avi');
-writerObj = VideoWriter(video_name);
-time_period = 'd'; %'1','d','m','y'
-set(writerObj,'FrameRate',datapoints/10); % 0.5 = 2 seconds per frame
-open(writerObj);
-
-case_name = strcat('cases/',case_name);
-plot_title_vec = " ";
-for i = 1:datapoints
-   % Get the file name
-   filename = strcat(case_name,"/history/iceh.",date,".nc");
-   % Plot the map
-   map_creator_miz(filename, plot_title_vec, i, variable, grid, map_type, user, case_name)
-   date = update_date(date);
-end    
-        
-
-    
-% iterate over each image
- for k=1:datapoints
-     % use imread to read the image
-     figname = sprintf('frames/image%d.png',k);
-     img = imread(figname);
-     % resize the imagei_vec
-     %img = imresize(img,2);
-     % convert the image to a frame using im2frame
-     frame = im2frame(img);
-     % write the frame to the video
-     writeVideo(writerObj,frame);
- end
-
- % close the writer
- close(writerObj);
- 
-
-%% Ice edge
-edge = ice_edge(case_name,date,grid);
-edge_data = zeros(size(lon));
-for i = 1:len
-    edge_data(i,edge(i)) = 1.0;
+    % Combining the datasets 
+    total_miz = fsd_miz;% + swh_miz; %+ dafsd_miz;
+    plot_map(lat,lon,total_miz,latshelf,lonshelf,shelf,text,1)
 end
 
-total_miz = total_miz + edge_data;
-%% Mapping
+% CICE data at each day during the season - movie
+if movie_miz == 1
+    figure(3)
+    video_name = strcat(variable, '_', case_name, '_', '2021_11_30', '.avi');
+    writerObj = VideoWriter(video_name);
+    time_period = 'd'; %'1','d','m','y'
+    set(writerObj,'FrameRate',datapoints/10); % 0.5 = 2 seconds per frame
+    open(writerObj);
 
-latitude = [-90,-30];
-longitude = [-180,180];
-figure(1)
-w = worldmap('world');
-    axesm eqaazim; %, eqaazim eqdazim vperspec, eqdazim flips the x-axis, and y-axis to eqaazim. cassini
-    setm(w, 'Origin', [-90 0 0]);
-    setm(w, 'maplatlimit', [-90,-30]);
-    setm(w, 'maplonlimit', [-180,180]);
-    setm(w, 'meridianlabel', 'on')
-    setm(w, 'parallellabel', 'off')
-    setm(w, 'mlabellocation', 30);
-    setm(w, 'plabellocation', 10);
-    setm(w, 'mlabelparallel', -45);
-    setm(w, 'grid', 'on');
-    setm(w, 'labelrotation', 'on')
-    pcolorm(lat,lon,total_miz)
-    land = shaperead('landareas', 'UseGeoCoords', true);
-    geoshow(w, land, 'FaceColor', [0.5 0.7 0.5])
-    pcolorm(latshelf,lonshelf,shelf)  
-    colorbar
-    text = strcat("The ", season, " marginal ice zone in 2005");
-    title(text,'interpreter','latex','FontSize', 18)
+    case_name = strcat('cases/',case_name);
+    plot_title_vec = " ";
+    for i = 1:datapoints
+       % Get the file name
+       filename = strcat(case_name,"/history/iceh.",date,".nc");
+       % Plot the map
+       map_creator_miz(filename, plot_title_vec, i, variable, grid, map_type, user, case_name)
+       date = update_date(date);
+    end    
+
+    % iterate over each image
+     for k=1:datapoints
+         % use imread to read the image
+         figname = sprintf('frames/image%d.png',k);
+         img = imread(figname);
+         % resize the imagei_vec
+         %img = imresize(img,2);
+         % convert the image to a frame using im2frame
+         frame = im2frame(img);
+         % write the frame to the video
+         writeVideo(writerObj,frame);
+     end
+
+     % close the writer
+     close(writerObj);
+end % if movie
+
+
+%% 2. Proportion of winter that cells satisy the FSD requirements over winter
+if sum_miz_switch == 1
+    date = sprintf('%d-0%d-0%d', year, month, day);
+    for i = 1:datapoints
+       % Get the file name
+       filename = strcat(case_name,"/history/iceh.",date,".nc");
+       filename = strcat("cases/",case_name,"/history/iceh.",date,".nc");
+       data = ncread(filename, variable); % wave_sig_ht, dafsd_wave, fsdrad, peak_period
+       data = rearrange_matrix(data,37,dim);
+       data_formatted = [data; data(end,:)];
+       idx = data_formatted > fsd_max; 
+       fsd_miz = data_formatted;
+       fsd_miz(idx) = 0.0;
+       idx = fsd_miz > eps;
+       fsd_miz(idx) = 1.0;
+       total_data(:,:,i) = fsd_miz;
+       date = update_date(date);
+    end    
+    sum_miz = sum(total_data,3)/datapoints;
+    fsdradius = sprintf("with floe radius less than %d m", fsd_max);
+    text = strcat("Proportion of days with ",fsdradius, " during ",season, " in 2005");
+    plot_map(lat,lon,sum_miz,latshelf,lonshelf,shelf,text,2)
+end
+
+%% 3. Average SWH within sea ice regions (greater than 15% SIC)
+
+if swh_ice_miz_switch == 1
+   date = sprintf('%d-0%d-0%d', year, month, day);
+% a) Find the ice edge
+    mask = ice_mask(case_name,date,grid);
+
+% b) SWH > 0
+    dim = 2;
+    variable = "wave_sig_ht";
+    swh_data = aggregate_data(case_name,date,datapoints,variable,dim);
+    idx = swh_data < eps;
+    swh_miz = swh_data;
+    swh_miz(idx) = 0.0;
+    
+    % only print SWH in areas where there is sea ice
+    swh_miz = swh_miz.*mask;
+    text = "SWH $ > 0$ m with SIC $> 0.15$ averaged over winter 2005";
+    plot_map(lat,lon,swh_miz,latshelf,lonshelf,shelf,text,3)
+end
+%% Ice edge
+if iceedge == 1
+    edge = ice_edge(case_name,date,grid);
+    edge_data = zeros(size(lon));
+    for i = 1:len
+        edge_data(i,edge(i)) = 1.0;
+    end
+    figure(3)
+    plot_map(lat,lon,edge_data,latshelf,lonshelf,shelf,text,3)
+end
+
+%% Correlation between FSD and SWH MIZ definitions
+
+filename = strcat("cases/",case_name,"/history/iceh.",date,".nc");
+land_mask = ncread(filename, "tmask");
+land_mask = rearrange_matrix(land_mask,37,dim);
+combined_data = swh_miz;%~land_mask*NaN;
+correlation_data = swh_miz;
+idx = swh_miz > eps;
+combined_data(idx) = 0.0;
+correlation_data(idx) = 0.0;
+count1= 0;
+count2 = 0;
+
+if swh_ice_miz_switch == 1
+    if sum_miz_switch == 1
+        [len, wid] = size(swh_miz);
+        for i = 1:len
+            for j = 1:wid
+                if isnan(sum_miz(i,j)) == 0
+                    if sum_miz(i,j) > eps && swh_miz(i,j) > eps
+                        combined_data(i,j) = 1.0;
+                    else
+                        combined_data(i,j) = 0.0;
+                    end
+
+                    if sum_miz(i,j) < eps && swh_miz(i,j) < eps
+                        correlation_data(i,j) = 1.0;
+                        count1 = count1 + 1;
+                    elseif sum_miz(i,j) > eps && swh_miz(i,j) > eps
+                        correlation_data(i,j) = 1.0;
+                        count2 = count2 + 1;
+                    else
+                        correlation_data(i,j) = 0.0;
+                    end
+                end
+            end
+        end
+        figure(4)
+        text = "Combined MIZ";
+        plot_map(lat,lon,combined_data,latshelf,lonshelf,shelf,text,4)
+
+        text = "Correlation between the two MIZ definitions";
+        plot_map(lat,lon,correlation_data,latshelf,lonshelf,shelf,text,5)
+    end
+end
+
+
+% Correlation
+nan_idx = isnan(correlation_data);
+
+same_count = sum(sum(correlation_data(~nan_idx)));
+total = numel(correlation_data(~nan_idx));
+
+correlation = same_count/total;
+fprintf("The correlation between the two MIZ definitions is: %f \n",correlation);
+
+
+% FSD - SWH
+
+diff_data = sum_miz - swh_miz;
+text = "Difference between FSD amd SWH MIZs";
+ plot_map(lat,lon,diff_data,latshelf,lonshelf,shelf,text,6)
 
 %% MIZ width
  
 %% Printing ice area
-% figure(2)
-% dim = 2;
-% variable = "aice";
-% aice_data = aggregate_data(case_name,date,datapoints,variable,dim);
+%   figure(4)
+%  dim = 2;
+%  variable = "aice";
+%  aice_data = aggregate_data(case_name,date,datapoints,variable,dim);
 % 
 % latitude = [-90,-30];
 % longitude = [-180,180];
@@ -187,14 +280,14 @@ w = worldmap('world');
 %     setm(w, 'mlabelparallel', -45);
 %     setm(w, 'grid', 'on');
 %     setm(w, 'labelrotation', 'on')
-%     pcolorm(lat,lon,aice_data)
+%     pcolorm(lat,lon,correlation_data)
 %     land = shaperead('landareas', 'UseGeoCoords', true);
 %     geoshow(w, land, 'FaceColor', [0.5 0.7 0.5])
 %     pcolorm(latshelf,lonshelf,shelf)  
 %     colorbar
 %     text = strcat("The ", season, " marginal ice zone in 2005");
 %     title(text,'interpreter','latex','FontSize', 18)
-    
+%     
 %% Functions
 
 function ave_data = aggregate_data(case_name,date,datapoints,variable,dim)
@@ -240,6 +333,27 @@ function edge = ice_edge(case_name,date,grid)
         ice_pos(i) = pos(1);
     end
     edge = ice_pos;
+end
+
+
+function mask = ice_mask(case_name,date,grid)
+    %% Find the ice edge
+    % Find the ice edge and land edge
+    filename = strcat("cases/",case_name,"/history/iceh.",date,".nc");
+    latit = 100;
+    variable = "aice";
+    dim = 2;
+    datapoints = 92;
+    [lat,lon,row] = grid_read(grid);
+    aice_data(:,:) = aggregate_data(case_name,date,datapoints,variable,dim);
+    [len, wid] = size(aice_data);
+    ice_pos = zeros(len,wid);
+    for i = 1:len
+        long_ice_edge = aice_data(i,1:latit);
+        pos = find(long_ice_edge > 0.15);
+        ice_pos(i,pos) = 1;
+    end
+    mask = ice_pos;
 end
 
 function y = map_creator_miz(filename, plot_title_vec, i, variable, grid, map_type, user, case_name)
@@ -398,4 +512,30 @@ end
     %'/Users/noahday/MATLAB-Drive/MATLAB/PhD Project/CICE Plotting/frames';
     saveas(gcf,fullfile(filedir, figname));
 end
+end
+
+function [] = plot_map(lat,lon,total_miz,latshelf,lonshelf,shelf,text,i)
+    %% Mapping
+    latitude = [-90,-30];
+    longitude = [-180,180];
+    figure(i)
+    w = worldmap('world');
+        axesm eqaazim; %, eqaazim eqdazim vperspec, eqdazim flips the x-axis, and y-axis to eqaazim. cassini
+        setm(w, 'Origin', [-90 0 0]);
+        setm(w, 'maplatlimit', [-90,-30]);
+        setm(w, 'maplonlimit', [-180,180]);
+        setm(w, 'meridianlabel', 'on')
+        setm(w, 'parallellabel', 'off')
+        setm(w, 'mlabellocation', 30);
+        setm(w, 'plabellocation', 10);
+        setm(w, 'mlabelparallel', -45);
+        setm(w, 'grid', 'on');
+        setm(w, 'labelrotation', 'on')
+        pcolorm(latshelf,lonshelf,shelf)
+        pcolorm(lat,lon,total_miz)
+        land = shaperead('landareas', 'UseGeoCoords', true);
+        geoshow(w, land, 'FaceColor', [0.5 0.7 0.5])  
+        colorbar
+        title(text,'interpreter','latex','FontSize', 18)
+        set(gcf, 'Position',  [100, 100, 1000, 800])
 end
