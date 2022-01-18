38f9d0d481fe93b33c9aaec996606aeb2c1cf0ba
Noah Day <noah.day@adelaide.edu.au>
2022-01-19 08:12:35 +1030

    adding cases

--- initial_ic.m
+++ initial_ic.m
@@ -7,9 +7,15 @@ clear
 close all
 addpath functions
 filedir = 'cases/init/history/iceh_ic.2005-01-01-03600.nc';
+% Files
+% Initial conditions forcing: iced_gx1_v6.2005-01-01.nc
+% Initial conditions of CICE: iceh_ic.2005-01-01-03600.nc
+% First timestep (1 hour): iceh_inst.2005-01-01-03600.nc
+% After one day: iceh.2005-01-01.nc
 ncdisp(filedir)
 
 % Parameters
+fstd_switch = 0;
 coords = [-45,20;-65,20;-45,40;-65,40]; %(NW;SW,NE,SE)
 grid = 'gx1';
 %% Retreive the grid.
@@ -17,28 +23,22 @@ grid = 'gx1';
 % for i = 1:4
 %     [lat_out(i),lon_out(i)] = lat_lon_finder(coords(i,1),coords(i,2),lat,lon);
 % end
-data = data_format(filedir,'aicen_d',row,lat,lon,4);
-[lat_out,lon_out] = lat_lon_finder(coords(2,1),coords(2,2),lat,lon);
-fstd = data(lon_out,lat_out,:,:);
-size(sum(fstd))
+if fstd_switch == 1
+    data = data_format(filedir,'aicen_d',row,lat,lon,4);
+    [lat_out,lon_out] = lat_lon_finder(coords(2,1),coords(2,2),lat,lon);
+    fstd = data(lon_out,lat_out,:,:);
+    size(sum(fstd))
+end
 
-% % Vertices
-% for i = 1:4
-%     data(lon_out(i),lat_out(i)) = 10;
-% end
-% % Edges
-% % North latitude
-% data(lon_out(1):lon_out(3), lat_out(1):lat_out(3)) = 10;
-% % South latitude
-% data(lon_out(2):lon_out(4), lat_out(2):lat_out(4)) = 10;
-% % East longitude
-% data(lon_out(2):lon_out(1), lat_out(2):lat_out(1)) = 10;
-% % West longitude
-% data(lon_out(4):lon_out(3), lat_out(4):lat_out(3)) = 10;
 %% Begin mapping.
 figure(1)
-data = data_format(filedir,'fsdrad',row,lat,lon,4);
-data(lon_out,lat_out) = 10;
+data = data_format(filedir,'afsd',row,lat,lon,3); %aicen
+for i = 1:24
+    idx = data(:,:,i)> 0;
+    sum(sum(idx))
+end
+%data(lon_out,lat_out) = 10;
+data = data(:,:,10);
 color_map = seaicecolormap();
 latitude = [-90,-60];
 longitude = [10,50];
